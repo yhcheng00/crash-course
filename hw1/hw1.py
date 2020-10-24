50151686dd0afef973b3dfaccfa990a24738b226
Calvin Yan <calvinyan@berkeley.edu>
2020-10-24 10:43:53 -0700

    Add homework 1

---
+++ hw1/hw1.py
@@ -0,0 +1,32 @@
+# Goal: Given an input image with five randomly positioned pure colors, ascertain
+# the positions of these colors and return their values in various formats
+import cv2
+import numpy as np
+
+# For each function, return a list of color representations of the corresponding
+# point (1 for getColorOne, 2 for getColorTwo...) in the following format:
+# [[R, G, B], [B, G, R], [H, S, V], grayscale]
+
+def getColorOne(img):
+    # TODO: Replace this line
+
+def getColorTwo(img):
+    # TODO: Replace this line
+
+def getColorThree(img):
+    # TODO: Replace this line
+
+def getColorFour(img):
+    # TODO: Replace this line
+
+def getColorFive(img):
+    # TODO: Replace this line
+
+
+if name == '__main__':
+    # Do any testing you need to do here! I suggest opening the image in OpenCV
+    # and plugging it into your functions to start with
+
+# SUBMISSION INSTRUCTIONS:
+# Upload a zip file containing hw1.py and hw1.jpg to the Gradescope portal.
+# Do not modify the names in any way.
